ab5608e686e5dc9bac3c2f8d1ad22d05306dd063
Varundatt2824 <varundatttiwari@gmail.com>
2024-01-22 01:24:07 +0530

    updated prompt

--- utils/category_extraction.py
+++ utils/category_extraction.py
@@ -10,12 +10,24 @@ def category_extraction(llm,product_list,summary):
             SystemMessage
         )
     chat_messages=[
-    SystemMessage(content='You are an multilingual expert in understanding regulatory documents.\
-                           You are given a list of product and summary of regulatory document.\
-                           Given the regulation text from pdf you are suppose to output a list of product categories which are relavant to this text:\
-                           Give the output in a with their relavance score and a reason for why they are the best choices \n\
-                           The products should be strictly coming from the product list given\
-                           The output should be in json format'),
+    SystemMessage(content="""You are an multilingual expert in understanding regulatory documents.
+                           You are given a list of product and summary of regulatory document.
+                           Given the regulation text from pdf you are suppose to output a list of product categories which are relavant to this text:
+                           Give the output in a with their relavance score and an explaination for choosing that category \n
+                           The products should be strictly coming from the product list given
+                           The output should be in json format
+                  An example of output is given below
+                        {relavant products:[
+                            0:{product:
+                                score:
+                                reason:
+                            },
+                            1:{product:
+                                score:
+                                reason:
+                            },
+                        ] 
+                        } """),
     HumanMessage(content=f'List of products are : {product_list}\n\
                  summary: {summary}')
     ]
